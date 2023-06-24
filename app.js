1f49ba517409f9ca6a16e1215853270d4670089c
SimpleMan2022 <aditn689@gmail.com>
2023-06-24 11:20:21 +0700

    cek koneksi y/n

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const {MongoClient} = require("mongodb")
+
+const uri = "mongodb://127.0.0.1:27017"
+const dbName = "belajar-nodejs"
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+
+client.connect((err, res) => {
+    if(err) {
+       return console.log('koneksi gagal!')
+    }
+
+    console.log('koneksi berhasil')
+})
