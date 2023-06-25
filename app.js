662693574ea4a16c6b217aa833bebfc72ecb1fca
SimpleMan2022 <aditn689@gmail.com>
2023-06-25 13:58:21 +0700

    menyelesaikan crud contact-app menggunakan mongodb

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const ejs = require('ejs')
 const expressLayouts = require('express-ejs-layouts')
+const { body, validationResult, check } = require('express-validator');
+const methodOverride = require('method-override')
 const app = express()
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
@@ -21,6 +23,7 @@ app.use(session({
     saveUninitialized : true
 }))
 app.use(flash())
+app.use(methodOverride('_method'))
 
 
 const port = 3000
@@ -45,6 +48,116 @@ app.get('/contact', async (req, res) => {
         contacts,
         msg : req.flash('msg')
     })
+})
+
+app.get('/contact/add', (req, res) => {
+    res.render('addContact', {
+        layout : 'layouts/main',
+        title : 'Halaman tambah data contact | nodejs',
+        // contacts
+    })
+})
+
+app.post('/contact', 
+    [
+        body('nama').custom (async (value) => {
+            const duplicate = await Contact.findOne({nama : value})
+            if(duplicate){
+                throw new Error('Nama sudah terdaftar!')
+            }
+            return true
+        }),
+        check('email', 'Email tidak Valid!').isEmail(),
+        check('nohp', 'no hp tidak valid!').isMobilePhone('id-ID')
+
+    ]
+, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+       
+        res.render('addContact', {
+            layout : 'layouts/main',
+            title : 'Halaman tambah data contact | nodejs',
+            errors : errors.array()
+        })
+    }else{
+        Contact.insertMany(req.body)    
+        req.flash('msg', 'Data contact berhasil ditambahkan!')
+        res.redirect('/contact')
+    }
+    
+})
+
+// app.get('/contact/delete/:nama', async (req, res) => {
+//     const contact = await Contact.findOne({nama : req.params.nama})
+//     if(!contact){
+//         res.status(404)
+//         res.send('<h1>404</h1>')
+//     }else{
+//     Contact.deleteOne({_id : contact._id}).then((result) => {
+//         req.flash('msg', 'data berhasil dihapus!')
+//         res.redirect('/contact')
+//     })
+    
+// }
+// })
+
+app.delete('/contact', (req, res) => {
+         Contact.deleteOne({nama : req.body.nama}).then((result) => {
+            req.flash('msg', 'data berhasil dihapus!')
+            res.redirect('/contact')
+     })
+})
+
+
+app.get('/contact/edit/:nama', async (req, res) => {
+    const contact = await Contact.findOne({nama : req.params.nama})
+
+    res.render('editContact', {
+        layout : 'layouts/main',
+        title : 'Halaman Edit data contact | nodejs',
+        contact
+    })
+})
+
+app.put('/contact', 
+    [
+        body('nama').custom(async (value, {req}) => {
+            const duplicate = await Contact.findOne({nama : value})
+            if(value !== req.body.oldName && duplicate){
+                throw new Error('Nama sudah terdaftar!')
+            }
+            return true
+        }),
+        check('email', 'Email tidak Valid!').isEmail(),
+        check('nohp', 'no hp tidak valid!').isMobilePhone('id-ID')
+
+    ]
+, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+       
+        res.render('editContact', {
+            layout : 'layouts/main',
+            title : 'Halaman tambah data contact | nodejs',
+            errors : errors.array(),
+            contact : req.body
+        })
+    }else{
+        Contact.updateOne(
+            {_id : req.body._id},
+            {
+                $set : {
+                    nama : req.body.nama,
+                    email : req.body.email,
+                    nohp : req.body.nohp
+                }
+            }).then((result)=> {
+                req.flash('msg', 'data berhasil diupdate')
+                res.redirect('/contact')
+            })
+    }
+    
 })
 
 app.get('/contact/:nama', async (req, res) => {
